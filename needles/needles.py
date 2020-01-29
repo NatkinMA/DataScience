915c0b93e8383ed954e4eba2a2fad928260f566d
Michael <NatkinMA@ya.ru>
2020-01-29 14:31:38 +0700

    Needles версия 0.3 - вызов функций get_time_for_find_needles_in_stack() помещен в цикл; - сформирован список, содержащий list, tuple и dict, в цикле проходится этот список и вызывается функция time_for_find_needles_in_stack(); - набор "иголок" формируется более простым способом; - не используемая переменная цикла заменена на нижнее подчеркивание; - для поиска "иголок" в "стоге" выполняется выражение needle in stack.

--- needles/needles.py
+++ needles/needles.py
@@ -28,6 +28,30 @@ time_end = timeit.default_timer() - time_start
 1) Ограничить размер набора "иголок" 1 000 элементов.
 2) расчеты производить для: 1 000, 10 000, 100 000, 1 000 000 элементов.
 Расчета для 10 000 000 производить не нужно.
+
+На счет задачи поиска иголок:
+- вызов функции get_time_for_find_needles_in_stacks() можно поместить в цикл.
+Например:
+STACK_LENGTH = [1000, 10000, 100000, 1000000]
+NEEDLES_LENGTH = 1000
+...
+for n in STACK_LENGTH:
+  get_time_for_find_needles_in_stacks(n, NEEDLES_LENGTH)
+- то же самое с вызовом функции  time_for_find_needles_in_stack, можно сформировать
+список содержащий list, tuple и dict. И в цикле пройтись по этому списку и вызвать функцию.
+- можно использовать needles.append(random.choice(stack_list)), вместо:
+k = random.randint(0, (stack_size - 1))
+needles.append(stack_list[k])
+- или более просто сформировать набор иголок:
+needles = [random.choice(stack_list) for _ in range(needles_length//2)]
+needles.extend([random.uniform(1, 2) for _ in range(needles_length//2)])
+- если в цикле не используется переменная цикла, ее лучше заменить на нижний подчкерк
+например, [random.random() for _ in range(stack_size)]
+- для поиска иголок в стоге достаточно было обойти в цикле все иголки и выполнить выражение needle in stack.
+например,
+for n in needles:
+  if n in stask:
+    pass
 '''
 
 import random
@@ -35,40 +59,32 @@ import timeit
 import sys
 
 
-def time_for_find_needle_in_stack(stack, needle):
-    time_start = timeit.default_timer()
-    for element in stack:
-        if element == needle:
-            break
-    time_end = timeit.default_timer() - time_start
-    print('Stack size:', sys.getsizeof(stack), 'Time: ', time_end)
-
-
 def time_for_find_needles_in_stack(stack, needles):
+    time_start = timeit.default_timer()
+    i = 0
     for needle in needles:
-        time_for_find_needle_in_stack(stack, needle)
+        if needle in stack:
+            pass
+    time_end = timeit.default_timer() - time_start
+    return sys.getsizeof(stack), time_end
 
 
 def get_time_for_find_needles_in_stacks(stack_size, needles_size):
+    stacks = []
     stack_list = [random.random() for i in range(stack_size)]
+    stacks.append(stack_list)
     stack_tuple = tuple(stack_list)
+    stacks.append(stack_tuple)
     stack_dict = dict(zip(stack_list, [1]*len(stack_list)))
-
-    needles = []
-    for i in range(needles_size):
-        if i <= 500:
-            k = random.randint(0, (stack_size - 1))
-            needles.append(stack_list[k])
-        else:
-            needles.append(random.uniform(1, 2))
-
-    time_for_find_needles_in_stack(stack_list, needles)
-    time_for_find_needles_in_stack(stack_tuple, needles)
-    time_for_find_needles_in_stack(stack_dict, needles)
+    stacks.append(stack_dict)
+    needles = [random.choice(stack_list) for _ in range(needles_size // 2)]
+    needles.extend([random.uniform(1, 2) for _ in range(needles_size // 2)])
+    for current_stack in stacks:
+        print(time_for_find_needles_in_stack(current_stack, needles))
 
 
 if __name__ == '__main__':
-    get_time_for_find_needles_in_stacks(1000, 1000)
-    get_time_for_find_needles_in_stacks(10000, 1000)
-    get_time_for_find_needles_in_stacks(100000, 1000)
-    get_time_for_find_needles_in_stacks(1000000, 1000)
+    STACK_LENGTH = [1000, 10000, 100000, 1000000]
+    NEEDLES_LENGTH = 1000
+    for n in STACK_LENGTH:
+        get_time_for_find_needles_in_stacks(n, NEEDLES_LENGTH)
